d0d1f51ed76cab5525eaf4e98797d583d0ad1b19
James Foucar <jgfouca@sandia.gov>
2020-04-10 17:51:07 -0400

    Needed to use e3sm_add_flags CMake function

--- cime/config/e3sm/machines/Depends.summit.pgigpu.cmake
+++ cime/config/e3sm/machines/Depends.summit.pgigpu.cmake
@@ -36,5 +36,5 @@ set(FILES_NEED_OPENACC_FLAGS
 )
 
 foreach(ITEM IN LISTS FILES_NEED_OPENACC_FLAGS)
-  set_property(SOURCE ${ITEM} APPEND_STRING PROPERTY COMPILE_FLAGS "-Minline -ta=nvidia,cc70,fastmath,loadcache:L1,unroll,fma,managed,ptxinfo -Mcuda -Minfo=accel")
+  e3sm_add_flags("${ITEM}" "-Minline -ta=nvidia,cc70,fastmath,loadcache:L1,unroll,fma,managed,ptxinfo -Mcuda -Minfo=accel")
 endforeach()
